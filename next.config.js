b7c3159e05285a86f0f596d389192b107d529ec6
Jesse Farmer <jesse@20bits.com>
2023-05-06 11:34:01 -0700

    add kv and enable RSC

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compress: false,
+  experimental: {
+    serverActions: true,
+  }
+}
 
 module.exports = nextConfig
